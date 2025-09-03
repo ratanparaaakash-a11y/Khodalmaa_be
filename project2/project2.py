058a2c79b95effaaa636fa9d7e33d1bc207a1471
abbas <abbas.murudkar@elixirequities.com>
2025-09-04 00:35:06 +0530

    backend changes

--- project2/project2.py
+++ project2/project2.py
@@ -9,25 +9,30 @@ connections_project2 = []
 async def get_p2_data(req: Request):
     try:
         data = await req.json()
-        print(f"project2 data: {data}")
+        print(f"project2 raw data: {data}")
 
-        # Merge arrays per machine
+        filtered_data = {}
         for machine_id, values in data.items():
+            if any(v > 100000 for v in values):
+                print(f"Skipping {machine_id} due to large value")
+                continue
+            filtered_data[machine_id] = values
             latest_project2_data[machine_id] = values
 
         for conn in connections_project2:
             try:
-                await conn.send_json(data)
+                await conn.send_json(filtered_data)
             except Exception as e:
                 print(f"Error sending to WebSocket: {e}")
 
-        return {"status": "success", "data": data}
+        return {"status": "success", "data": filtered_data}
     except Exception as e:
-        print(f"An Error occurred on our site project1 {str(e)}")
+        print(f"An Error occurred on our site project2 {str(e)}")
         raise HTTPException(status_code=500, detail=str(e))
-    
+
+
 @Project2Router.websocket("/ws_project2")
-async def ws_project1(websocket: WebSocket):
+async def ws_project2(websocket: WebSocket):
     await websocket.accept()
     connections_project2.append(websocket)
     print("Frontend connected to Project2 WS")
